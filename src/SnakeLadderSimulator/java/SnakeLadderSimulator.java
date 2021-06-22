798499987078d51c5d9b0d4253aa94eb78bd4332
om9022 <omkarchikane607@gmail.com>
2021-06-22 18:37:08 +0530

    Updated: check that palyer gets exact winning position 100

--- src/SnakeLadderSimulator/java/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator/java/SnakeLadderSimulator.java
@@ -2,51 +2,54 @@ package SnakeLadderSimulator.java;
 
 public class SnakeLadderSimulator {
 public static void main(String[] args) {
-	System.out.println("Welcome to Snake Ladder Simulator");
-	int START_POSITION = 0;
+	System.out.println( "WelCome To Snake Ladder Simulator" );
 //	CONSTANTS
-	final int NO_PLAY = 0;
-	final int LADDER = 1;
-	final int SNAKE = 2;
-	final int GOAL = 100;
-	
-//	variable
-	int PlayerPosition = 0;
-	
-//	CALCULATION
-	while(PlayerPosition <= GOAL)
-	{
-		
-		int DiceRoll = (int) (Math.floor(Math.random() * 10 ) % 6) +1;
-		int PlayerOption = (int) Math.floor(Math.random() * 10 ) % 3;
-		
-		
-		System.out.println("Dice value "+ DiceRoll);
-		System.out.println("PlayerOption" + PlayerOption );
+	int START_POSITION = 0;
+	final int NO_PLAY = 0 ;
+	final int LADDER = 1 ;	
+	final int SNAKE = 2 ;
+	final int GOAL = 100 ;
 
-		switch ( PlayerOption) {
+//	variable
+	int PlayerPosition = 0 ;
+	
+//	EXPRESIONS
+	while( PlayerPosition < GOAL ) 
+	{
+	
+		int DiceRoll = (int) (Math.floor(Math.random() * 10 ) % 6) + 1 ;
+		int PlayerOption = (int) Math.floor(Math.random() * 10 ) % 3 ;
+		
+		System.out.println("Dice value " + DiceRoll);
+		System.out.println( "Player option " + PlayerOption);
+		
+		switch ( PlayerOption )
+		{
+		
 		case LADDER :
-			PlayerPosition = PlayerPosition + DiceRoll;
-			break;
-		case SNAKE :
-			if ((PlayerPosition -DiceRoll) <= START_POSITION )
+			if (( PlayerPosition + DiceRoll ) <= GOAL)
 			{
-				PlayerPosition = START_POSITION;
+			PlayerPosition = PlayerPosition + DiceRoll;
+			}
+			break;
+			
+		case SNAKE :		
+			
+			if ( ( PlayerPosition - DiceRoll ) <= START_POSITION )
+			{
+				PlayerPosition = START_POSITION ;
 			}
 			else
-			{
-				PlayerPosition = PlayerPosition - DiceRoll;
+			{				
+				PlayerPosition = PlayerPosition - DiceRoll ;
 			}
 			break;
-			
-		default :
+	
+		default:
 			
 			break;
-		
 		}
-		System.out.println("Player Position" +PlayerPosition + "\n ---------");
+		System.out.println("Player Position " + PlayerPosition + "\n --------");
 	}
-
-	}
-
+ }
 }
