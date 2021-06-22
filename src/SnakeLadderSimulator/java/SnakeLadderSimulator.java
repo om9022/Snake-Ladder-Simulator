2978bda905b1678fc8f8f971a7fdd17b23ab205d
om9022 <omkarchikane607@gmail.com>
2021-06-22 17:26:19 +0530

    Updated: A die to get number 1 to 6

--- src/SnakeLadderSimulator/java/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator/java/SnakeLadderSimulator.java
@@ -4,8 +4,9 @@ public class SnakeLadderSimulator {
 public static void main(String[] args) {
 	System.out.println("Welcome to Snake Ladder Simulator");
 	int START_POSITION = 0;
-	System.out.println(START_POSITION);
+	
+	int DiceRoll = (int) (Math.floor(Math.random() * 10 ) % 6) +1;
+	System.out.println(DiceRoll );
 
 }
 }
-
