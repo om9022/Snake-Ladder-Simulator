3055b6ff4fc699faead25c9b90b11e8435aec81d
om9022 <omkarchikane607@gmail.com>
2021-06-22 17:47:11 +0530

    Updated: checking for player option that are noplay ladder or snake

--- src/SnakeLadderSimulator/java/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator/java/SnakeLadderSimulator.java
@@ -4,9 +4,31 @@ public class SnakeLadderSimulator {
 public static void main(String[] args) {
 	System.out.println("Welcome to Snake Ladder Simulator");
 	int START_POSITION = 0;
+//	CONSTANTS
+	final int NO_PLAY = 0;
+	final int LADDER = 1;
+	final int SNAKE = 2;
 	
+//	variable
+	int PlayerPosition = 0;
+	
+//	CALCULATIONS
 	int DiceRoll = (int) (Math.floor(Math.random() * 10 ) % 6) +1;
-	System.out.println(DiceRoll );
+	int PlayerOption = (int) Math.floor(Math.random() * 10 ) % 3;
+	
+	switch ( PlayerOption) {
+	case LADDER :
+		PlayerPosition = PlayerPosition + DiceRoll;
+		break;
+	case SNAKE :
+		PlayerPosition = PlayerPosition - DiceRoll;
+		break;
+		
+	default :
+		
+		break;
+	
+	}
+	}
 
-}
 }
