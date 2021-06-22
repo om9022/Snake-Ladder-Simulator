d5c8cf559eb19dfde5b8719a1e7149b3318bc180
om9022 <omkarchikane607@gmail.com>
2021-06-22 17:09:59 +0530

    Updated : set start positoon at 0

--- src/SnakeLadderSimulator/java/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator/java/SnakeLadderSimulator.java
@@ -3,6 +3,8 @@ package SnakeLadderSimulator.java;
 public class SnakeLadderSimulator {
 public static void main(String[] args) {
 	System.out.println("Welcome to Snake Ladder Simulator");
+	int START_POSITION = 0;
+	System.out.println(START_POSITION);
 
 }
 }
